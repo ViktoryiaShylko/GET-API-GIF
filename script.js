386130be12aea0716bf92b49693a0da23025d112
ViktoryiaShylko <vika.shylko@mail.ru>
2022-12-13 13:15:17 +0300

    added: function renderImages

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ btn.addEventListener("click", (e) => {
   e.preventDefault();
   let inputVal = input.value;
 
-  fetch(`https://api.giphy.com/v1/gifs/search?api_key=jeMvigjtlEAFHjrfJxcWres0ILHNxaIr&q=${inputVal}&limit=5&offset=0`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    data.data.forEach(element => {
-    array.push(element.images.original.url);
-    });
-    for(let i=0;i < array.length;i++){
-          result.innerHTML += `<img src="${array[i]}" alt = "">`;
-        };
-  })
-  .catch(error => console.log(error));
-});
+  fetch(`https://api.giphy.com/v1/gifs/search?api_key=jeMvigjtlEAFHjrfJxcWres0ILHNxaIr&q=${inputVal}&limit=5&offset=0&rating=g`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      renderImages(data);
+    })
+    .catch(error => console.log(error));
 
+  function renderImages(data) {
+    data.data.forEach(element => {
+      array.push(element.images.original.url);
+      });
+      for(let i=0;i < array.length;i++){
+        result.innerHTML += `<img src="${array[i]}" alt = "">`;
+      };
+  }
+});
